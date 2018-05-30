784f4485df9329b5239641f9390fd3880784f910
Aleksandra Marszalek <aaleksandra.mmarszalek@gmail.com>
2018-05-30 22:46:26 +0200

    base configuration add

---
+++ src/main/java/pl/coderslab/theultimatebet/TheUltimateBetApplication.java
@@ -0,0 +1,12 @@
+package pl.coderslab.theultimatebet;
+
+import org.springframework.boot.SpringApplication;
+import org.springframework.boot.autoconfigure.SpringBootApplication;
+
+@SpringBootApplication
+public class TheUltimateBetApplication {
+
+    public static void main(String[] args) {
+        SpringApplication.run(TheUltimateBetApplication.class, args);
+    }
+}
