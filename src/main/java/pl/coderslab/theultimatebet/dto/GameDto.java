51fd9b53f653c40449164ad925b131660bf7ad73
Aleksandra Marszalek <aaleksandra.mmarszalek@gmail.com>
2018-06-04 18:03:58 +0200

    game dto working

---
+++ src/main/java/pl/coderslab/theultimatebet/dto/GameDto.java
@@ -0,0 +1,120 @@
+package pl.coderslab.theultimatebet.dto;
+
+import com.fasterxml.jackson.annotation.JsonIgnoreProperties;
+import com.fasterxml.jackson.annotation.JsonProperty;
+
+import java.time.LocalDateTime;
+
+@JsonIgnoreProperties(ignoreUnknown = true)
+public class GameDto {
+
+    @JsonProperty("id")
+    private Long id;
+
+    @JsonProperty("team1_id")
+    private Long team1Id;
+
+    @JsonProperty("team2_id")
+    private Long team2Id;
+
+    @JsonProperty("pointsTeam1")
+    private int pointsTeam1;
+
+    @JsonProperty("pointsTeam2")
+    private int pointsTeam2;
+
+    @JsonProperty("oddsForTeam1")
+    private double oddsForTeam1;
+
+    @JsonProperty("oddsForTeam2")
+    private double oddsForTeam2;
+
+    @JsonProperty("gameTime")
+    private LocalDateTime gameTime;
+
+    @JsonProperty("status")
+    private int status;
+
+    @JsonProperty("signature")
+    private String signature;
+
+    public Long getId() {
+        return id;
+    }
+
+    public void setId(Long id) {
+        this.id = id;
+    }
+
+    public Long getTeam1Id() {
+        return team1Id;
+    }
+
+    public void setTeam1Id(Long team1Id) {
+        this.team1Id = team1Id;
+    }
+
+    public Long getTeam2Id() {
+        return team2Id;
+    }
+
+    public void setTeam2Id(Long team2Id) {
+        this.team2Id = team2Id;
+    }
+
+    public int getPointsTeam1() {
+        return pointsTeam1;
+    }
+
+    public void setPointsTeam1(int pointsTeam1) {
+        this.pointsTeam1 = pointsTeam1;
+    }
+
+    public int getPointsTeam2() {
+        return pointsTeam2;
+    }
+
+    public void setPointsTeam2(int pointsTeam2) {
+        this.pointsTeam2 = pointsTeam2;
+    }
+
+    public double getOddsForTeam1() {
+        return oddsForTeam1;
+    }
+
+    public void setOddsForTeam1(double oddsForTeam1) {
+        this.oddsForTeam1 = oddsForTeam1;
+    }
+
+    public double getOddsForTeam2() {
+        return oddsForTeam2;
+    }
+
+    public void setOddsForTeam2(double oddsForTeam2) {
+        this.oddsForTeam2 = oddsForTeam2;
+    }
+
+    public int getStatus() {
+        return status;
+    }
+
+    public void setStatus(int status) {
+        this.status = status;
+    }
+
+    public String getSignature() {
+        return signature;
+    }
+
+    public void setSignature(String signature) {
+        this.signature = signature;
+    }
+
+    public LocalDateTime getGameTime() {
+        return gameTime;
+    }
+
+    public void setGameTime(LocalDateTime gameTime) {
+        this.gameTime = gameTime;
+    }
+}
--- src/main/java/pl/coderslab/theultimatebet/service/GameServiceImpl.java
+++ src/main/java/pl/coderslab/theultimatebet/service/GameServiceImpl.java
@@ -1,15 +1,56 @@
 package pl.coderslab.theultimatebet.service;
 
 import org.springframework.beans.factory.annotation.Autowired;
+import org.springframework.http.ResponseEntity;
+import org.springframework.scheduling.annotation.Scheduled;
 import org.springframework.stereotype.Service;
+import org.springframework.web.client.RestTemplate;
+import pl.coderslab.theultimatebet.dto.GameDto;
+import pl.coderslab.theultimatebet.dto.TeamDto;
 import pl.coderslab.theultimatebet.entity.Game;
+import pl.coderslab.theultimatebet.entity.Team;
 import pl.coderslab.theultimatebet.repository.GameRepository;
+import pl.coderslab.theultimatebet.repository.TeamRepository;
 
 @Service
 public class GameServiceImpl implements GameService {
 
     @Autowired
     GameRepository gameRepository;
+
+    @Autowired
+    TeamRepository teamRepository;
+
+
+    /////////// getting from API /////////////////
+
+    @Scheduled(fixedRate = 1000*60)
+    public void getGamesFromApi() {
+        String url = "http://localhost:8090/game/";
+        RestTemplate restTemplate = new RestTemplate();
+        ResponseEntity<GameDto[]> responseGames = restTemplate.getForEntity(url, GameDto[].class);
+        GameDto[] games = responseGames.getBody();
+        for (GameDto game: games) {
+            Game g = new Game();
+            if (findById(game.getId()) != null) {
+                g = findById(game.getId());
+            }
+            g.setId(game.getId());
+            g.setGameTime(game.getGameTime());
+            g.setOddsForTeam1(game.getOddsForTeam1());
+            g.setOddsForTeam2(game.getOddsForTeam2());
+            g.setPointsTeam1(game.getPointsTeam1());
+            g.setPointsTeam2(game.getPointsTeam1());
+            g.setSignature(game.getSignature());
+            g.setStatus(game.getStatus());
+            g.setTeam1(teamRepository.findTeamById(game.getTeam1Id()));
+            g.setTeam2(teamRepository.findTeamById(game.getTeam2Id()));
+            gameRepository.save(g);
+        }
+    }
+
+
+    //////////// crud ////////////////
 
     @Override
     public Game findById(Long id) {
