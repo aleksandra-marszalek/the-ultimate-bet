1991bb729d10417584f021b90c924c501f42425e
Aleksandra Marszalek <aaleksandra.mmarszalek@gmail.com>
2018-06-06 19:26:42 +0200

    add basic tests

--- src/test/java/pl/coderslab/theultimatebet/repository/BetRepositoryTest.java
+++ src/test/java/pl/coderslab/theultimatebet/repository/BetRepositoryTest.java
@@ -1,11 +1,19 @@
 package pl.coderslab.theultimatebet.repository;
 
+import org.hamcrest.Matchers;
 import org.junit.Test;
 import org.junit.runner.RunWith;
 import org.springframework.beans.factory.annotation.Autowired;
 import org.springframework.boot.test.autoconfigure.orm.jpa.DataJpaTest;
 import org.springframework.boot.test.autoconfigure.orm.jpa.TestEntityManager;
 import org.springframework.test.context.junit4.SpringRunner;
+import pl.coderslab.theultimatebet.entity.*;
+
+import java.math.BigDecimal;
+import java.time.LocalDateTime;
+import java.util.ArrayList;
+import java.util.Collections;
+import java.util.List;
 
 import static org.junit.Assert.*;
 
@@ -22,18 +30,23 @@ public class BetRepositoryTest  {
 
 
     @Test
-    public void findBetById() {
+    public void findBetByIdShouldReturnNullWhenDatabaseEmpty() {
+        assertNull(betRepository.findBetById(1L));
     }
 
     @Test
-    public void findAllByUserIdOrderByCreatedDesc() {
+    public void findAllByUserIdOrderByCreatedDescShouldReturnNullWhenDatabaseEmpty() {
+        assertEquals(Collections.EMPTY_LIST, betRepository.findAllByUserIdOrderByCreatedDesc(1L));
     }
 
     @Test
-    public void findAllByResultOrderByCreatedDesc() {
+    public void findAllByResultOrderByCreatedDescShouldReturnNullWhenDatabaseEmpty() {
+        assertEquals(Collections.EMPTY_LIST, betRepository.findAllByResultOrderByCreatedDesc("won"));
     }
 
     @Test
-    public void findAllByUserIdAndResultOrderByCreatedDesc() {
+    public void findAllByUserIdAndResultOrderByCreatedDescShouldReturnNullWhenDatabaseEmpty() {
+        assertEquals(Collections.EMPTY_LIST, betRepository.findAllByUserIdAndResultOrderByCreatedDesc(1L, "won"));
     }
+
 }
